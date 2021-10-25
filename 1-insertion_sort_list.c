42b5a4059ccdaf15fe39fcdcc4346a5cc19800d0
Andres Diaz <andresdiaz5010@icloud.com>
2021-10-25 18:45:25 +0000

    Fix - 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux, *p;
 
-	if (*list == NULL || list == NULL)
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
 		return;
 	aux = (*list)->next;
 	while (aux != NULL)
